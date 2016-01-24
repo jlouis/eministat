e659ca8c82a78efcb361722be79d4631d857ec46
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2016-01-24 15:13:10 +0100

    Improve the EQC test case, find things.

--- test/eministat_eqc.erl
+++ test/eministat_eqc.erl
@@ -1,27 +1,31 @@
 -module(eministat_eqc).
 -include_lib("eqc/include/eqc.hrl").
 
+-compile(export_all).
+
 clevel() ->
     elements([80.0, 90.0, 95.0, 98.0, 99.0, 99.5]).
 
-input() -> oneof([integer(), real()]).
+input() -> oneof([choose(1,1000), real()]).
+           
+dataset(K) ->
+    ?LET([Prefix, L], [vector(K, input()), list(input())],
+       eministat_ds:from_list(dummy_ds, lists:append(Prefix, L))).
            
 dataset() ->
-    ?LET(Inputs, [input(), input(), input() | list(input())],
-         eministat_ds:from_list(
-           dummy_ds,
-           Inputs)).
+    ?LET(K, nat(),
+        dataset(K+3)).
 
-crashes(F) ->
+total(F) ->
     try
         F(), true
     catch
         Class:Reason ->
-            {error, Class, Reason}
+            {error, {Class, Reason}}
     end.
 
 prop_total() ->
     ?FORALL([CL, Base, Relatives], [clevel(), dataset(), list(dataset())],
-            not crashes(fun() ->
+            total(fun() ->
                                 eministat:a(CL, Base, Relatives)
                         end)).
